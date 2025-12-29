2794238c43284bd99f52c286c4f09ad6997698fc
Irfan Farooq <irfanf33@gmail.com>
2025-12-29 17:43:15 +0500

    changes to communicate with auth-service

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
 
+    /*
+
+    php artisan cache:clear
+    php artisan config:clear
+    php artisan config:cache
+
+    */
+
     Artisan::call('cache:clear');
     Artisan::call('config:clear');
     Artisan::call('config:cache');
